01d161b01bd8d5d562d4794728b0a8f28ba796af
Netanel Gilad <netanelg@wix.com>
2023-08-16 10:19:21 +0300

    remove implementations

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,13 @@
-import { OAuthStrategy, createClient } from '@wix/sdk';
-import { WIX_REFRESH_TOKEN_COOKIE } from 'lib/constants';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function middleware(request: NextRequest) {
-  const cookies = request.cookies;
   const res = NextResponse.next();
-  if (cookies.get(WIX_REFRESH_TOKEN_COOKIE)) {
-    return res;
-  }
-  const wixClient = createClient({
-    auth: OAuthStrategy({ clientId: process.env.WIX_CLIENT_ID! })
-  });
-  const tokens = await wixClient.auth.generateVisitorTokens();
-  res.cookies.set(WIX_REFRESH_TOKEN_COOKIE, JSON.stringify(tokens.refreshToken), {
-    maxAge: 60 * 60 * 24 * 30
-  });
+
+  // Fill in the body of this function
+  // In the middleware, we need to check if the request already has a visitor
+  // session cookie. If it does, we can continue running the request handler,
+  // otherwise we need to create a new visitor session and set the cookie.
+
   return res;
 }
 
